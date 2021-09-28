daa45f74a82a51b883d2502c5382f7cd03d3ef5b
Pramudia18 <90678507+Pramudia18@users.noreply.github.com>
2021-09-28 12:31:13 +0700

    Add files via upload

---
+++ Latihan.php
@@ -0,0 +1,11 @@
+<?php
+class  Contoh1 extends CI_Controller
+{ 
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Pramudia Permana
+            Saya tinggal di daerah Cikupa 
+            olah raga yang saya sukai adalah Basket";
+    }
+}
